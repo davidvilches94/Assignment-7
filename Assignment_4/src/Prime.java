ea220b25769414b4449f0cb91f3a973d7c50711e
davidvilches94 <davidvilches94@gmail.com>
2015-02-20 00:07:42 -0600

    added prime.java[incomplete]

---
+++ Assignment_4/src/Prime.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Prime {
+
+	public static void main (String [] args){
+		
+		int limit_value;
+		int prime_number;
+		int counter = 0;
+		 
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter a number. The system will find the prime numbers smaller than this number: ");
+		limit_value = input.nextInt();
+		
+		
+		for (prime_number = 2; prime_number <= limit_value; counter++)
+			
+			
+			
+			        
+			           
+			    
+		}
+		
+			
+			
+	}
+
