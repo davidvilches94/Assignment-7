4c80c53a32da5bda0749f76d161716e5905ad55a
aktunc-STMU <aktunc@gmail.com>
2015-03-22 10:28:57 -0500

    Do not copy or give out code to another student again.

--- Assignment_7/src/Coin.java
+++ Assignment_7/src/Coin.java
@@ -9,7 +9,7 @@ Coin(){
 
 void toss(){
 	double prob = Math.random();
-	if(prob > 0.4)
+	if(prob > 0.4) //this is not correct. It will give 60% heads, 40% tails.
 		sideUp = "heads";
 	else
 		sideUp = "tails";
